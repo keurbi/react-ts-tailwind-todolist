541582238b5278585d5ec40268331e5c511161a7
keurbi <desousa.loic8@gmail.com>
2024-10-31 13:45:29 +0100

    ajout select

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-interface Tasktodo {
+interface TasktodoInterface {
   title : string;
   index : number ;
   difficulty : number;
@@ -20,6 +20,28 @@ const Tasks: FC<TasksProps> = () => {
           name="task" 
           id="task"
         />
+        <select 
+          className='
+            w-3/12 
+            bg-gray-50 
+            border 
+            border-gray-300 
+            text-gray-900 
+            text-sm 
+            rounded-lg 
+            focus:ring-blue-500 
+            focus:border-blue-500 
+            block 
+            p-2.5 
+            dark:bg-gray-700 
+            dark:border-gray-600 
+            dark:text-white 
+            dark:focus:ring-blue-500 
+            dark:focus:border-blue-500'>
+          <option value="1">Facile</option>
+          <option value="2">Moyen</option>
+          <option value="3">Difficile</option>
+        </select>
       </div>
       <div className='h-[85%] mt-4 bg-slate-100 rounded-lg p-2'>
         contenu tasks
