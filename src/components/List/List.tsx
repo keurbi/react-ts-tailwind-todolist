080b1d699b53e6752b6bb83aaa2d1039620db32c
DE SOUSA Loic <lds@bourse.lu>
2024-10-31 12:21:36 +0100

    décomposition composants de la liste

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,6 @@
 import React, { FC, useState } from 'react';
+import Tasks from '../Tasks/Tasks';
+import Tasksdone from '../Tasksdone/Tasksdone';
 
 interface ListProps {}
 
@@ -7,35 +9,14 @@ const List: FC<ListProps> = () => {
   const [done, setDone] = useState([]);
 
   return (
-<div className="container flex flex-col h-2/4 bg-slate-300 rounded-lg border-2 border-transparent shadow-[0_35px_60px_-15px_rgba(2,2,0,0.3)] p-2">
-    <h1 className='h-[10%] font-mono text-4xl text-center underline decoration-pink-500 tracking-widest animate-slidein'> Yeepee</h1>
-    <div className='h-[90%] flex flex-row justify-center gap-4 p-2'>
-      <div className='w-6/12 h-full flex-nowrap bg-white rounded-lg border-transparent p-4 flex flex-col'>
-        <div className='flex gap-4 items-center h-[15%]'>
-          <h2 className='text-center text-2xl underline w-4/12'>Tasks to do</h2>
-          <input 
-            placeholder='Add a task' 
-            className='w-8/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' 
-            type="text" 
-            name="task" 
-            id="task"
-          />
-        </div>
-        <div className='h-[85%] mt-4 bg-slate-100 rounded-lg p-2'>Div des tasks à faire</div>
-      </div>
-      <div className='w-6/12 h-full flex-nowrap bg-white rounded-lg border-transparent p-4'>
-      <div className='flex gap-4 items-center h-[15%]'>
-          <h2 className='text-center text-2xl underline w-4/12'>Tasks done</h2>
-          <input 
-            placeholder='Look for a task done' 
-            className='w-8/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' 
-            type="text" 
-            name="task" 
-            id="task"
-          />
-        </div>
-        <div className='h-[85%] mt-4 bg-slate-100 rounded-lg p-2'>Div des tasks faites</div>
-      </div>
+    <div className="container flex flex-col h-2/4 bg-slate-300 rounded-lg border-2 border-transparent shadow-[0_35px_60px_-15px_rgba(2,2,0,0.3)] p-2">
+      <h1 className='h-[10%] font-mono text-4xl text-center underline decoration-pink-500 tracking-widest animate-slidein'>
+        Make ur tasks
+        <span className='animate-pulse font-bold'> NOW</span>
+      </h1>
+      <div className='h-[90%] flex flex-row justify-center gap-4 p-2'>
+        <Tasks />
+        <Tasksdone />
       </div>
     </div>
   );
--- src/components/Tasksdone/Tasksdone.tsx
+++ src/components/Tasksdone/Tasksdone.tsx
@@ -1,15 +1,17 @@
 import React, { FC } from 'react';
 
 
-interface TasksProps {}
+interface TasksdoneProps {}
 
-const Tasks: FC<TasksProps> = () => {
+const Tasksdone: FC<TasksdoneProps> = () => {
+
+
   return (
     <div className='w-6/12 h-full flex-nowrap bg-white rounded-lg border-transparent p-4 flex flex-col'>
       <div className='flex gap-4 items-center h-[15%]'>
-        <h2 className='text-2xl underline w-4/12 text-center'>Tasks to do</h2>
+        <h2 className='text-2xl underline w-4/12 text-center'>Tasks done</h2>
         <input 
-          placeholder='Add a task' 
+          placeholder='Look for a previous task' 
           className='w-8/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' 
           type="text" 
           name="task" 
@@ -17,10 +19,10 @@ const Tasks: FC<TasksProps> = () => {
         />
       </div>
       <div className='h-[85%] mt-4 bg-slate-100 rounded-lg p-2'>
-        contenu tasks
+        contenu tasks done
       </div>
     </div>
   );
 };
 
-export default Tasks;
+export default Tasksdone;
