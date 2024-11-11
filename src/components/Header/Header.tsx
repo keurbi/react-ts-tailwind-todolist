9a3f864c4a30637a8af77977e0955d30db8ddd9d
keurbi <desousa.loic8@gmail.com>
2024-11-11 16:49:56 +0100

    intégration reacthookform

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,49 +1,61 @@
 import React, { FC } from "react";
+import { useForm, SubmitHandler } from "react-hook-form"
 
 interface HeaderProps {
-
+  addTask : (taskData: {title : string, difficulty : number, status : string}) => void;
 }
 
-const Header: FC<HeaderProps> = () => (
-  <div className="flex gap-4 items-center h-[15%]">
-    <h2 className="text-2xl underline w-4/12 text-center">Manage your tasks</h2>
-    <input
-      value={""}
-      placeholder="Add a task"
-      className="w-8/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      type="text"
-      name="task"
-      id="task"
-    />
-    <select
-      value={""}
-      className="w-3/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    >
-      <option value="1">Facile 🟢</option>
-      <option value="2">Moyen 🟠</option>
-      <option value="3">Difficile 🔴</option>
-    </select>
-    <select >
-      <option value="todo">To do</option>
-      <option value="pending">Pending</option>
-      <option value="done">Done</option>
-    </select>
-    <button
-      className="
-        bg-blue-400 
-        hover:bg-blue-500 
-        text-white 
-        font-bold 
-        py-2 
-        px-4 
-        rounded 
-        focus:outline-none 
-        focus:shadow-outline
-      "
-    >
-      OK
-    </button>
-  </div>
-);
+type Inputs = {
+  title : string
+  difficulty : number
+  status : string
+}
+
+const Header: FC<HeaderProps> = ({ addTask }) => {
+  const {register, handleSubmit} = useForm<Inputs>();
+
+  return (
+    <div className="flex gap-4 items-center h-[15%]">
+      <h2 className="text-xl underline w-2/12 text-center">Manage your tasks</h2>
+      <form 
+              onSubmit={handleSubmit((data) => {
+                addTask(data)
+              })}
+        className="flex gap-4 items-center w-full"
+      >
+        <input 
+          {...register("title", {required: true})}
+          placeholder="Add a task"
+          className="w-8/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+        
+        <select 
+          {...register("difficulty", {required: true})}
+          className="w-3/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option value="1">Facile 🟢</option>
+          <option value="2">Moyen 🟠</option>
+          <option value="3">Difficile 🔴</option>
+        </select>
+
+        <select 
+          {...register("status", {required: true})}
+          className="w-3/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option value="todo">To do</option>
+          <option value="pending">Pending</option>
+          <option value="done">Done</option>
+        </select>
+
+        <button
+          type="submit"
+          className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          OK
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Header;
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,23 +1,32 @@
-import React, { FC, Fragment, useState } from "react";
+import React, { FC, Fragment, useCallback, useState } from "react";
 import Pagination from "../Pagination/Pagination";
 import DisplayTasks from "../DisplayTasks/DisplayTasks";
+import Header from "../Header/Header";
 
 interface ListProps {}
 
 const List: FC<ListProps> = () => {
 
-  const [tasks, setTasks] = useState([[],[],[]])
+  const tasksStates = ["todo","pending","done"];
+  const [tasks, setTasks] = useState([[],[],[]]);
+
+  const [title,setTitle] = useState("");
+  const [difficulty, setDifficulty] = useState(0);
+  const [status, setStatus] = useState();
+
+  const addTask = useCallback((taskData : {title :string, difficulty : number, status : string})=>{
+    newTask = {title : ""}
+  },[]);
 
 
   return (
-    <div className="container flex flex-col h-2/4 bg-slate-300 rounded-lg border-2 border-transparent shadow-[0_35px_60px_-15px_rgba(2,2,0,0.3)] p-2">
+    <div className="container flex flex-col h-3/4 bg-slate-300 rounded-lg border-2 border-transparent shadow-[0_35px_60px_-15px_rgba(2,2,0,0.3)] p-2">
       <h1 className="h-[10%] font-mono text-4xl text-center underline decoration-pink-500 tracking-widest animate-slidein">
         Make ur tasks:
         <span className="animate-pulse font-bold">NOW</span>
       </h1>
-      <div className="h-[90%] flex flex-row justify-center gap-4 p-2">
+      <Header addTask = {addTask}/>
        {/* <DisplayTasks/> */}
-      </div>
     </div>
   );
 };export default List;
